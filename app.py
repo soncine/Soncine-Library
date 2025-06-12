333f97ba9e7547470790623b977dacc8d76b5fd2
Enzo Soncine <soncineenzo@gmail.com>
2025-06-11 21:04:15 -0300

    Folders, routes and faces
    Troquei o nome das pastas pra ficarem mais no padrão de codigo, ajeitei a face do login e fiz a do register, alem de criar as rotas pra rodar no flask

--- app.py
+++ app.py
@@ -2,3 +2,23 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+    else:
+        return render_template('login.html')
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        confirmation = request.form.get('confirmation')
+    else:
+        return render_template('register.html')
